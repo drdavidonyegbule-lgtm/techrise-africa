930dd18a035409a88cef329dcbca0d5850f53088
drdavidonyegbule-lgtm <dr.davidonyegbule@gmail.com>
2026-07-01 16:33:26 +0100

    Update sitemap[.]xml.ts

--- src/routes/sitemap[.]xml.ts
+++ src/routes/sitemap[.]xml.ts
@@ -1,7 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
 import type {} from "@tanstack/react-start";
 
-const BASE_URL = "https://techrise-africa.lovable.app";
+const BASE_URL = "https://traacademy.org";
 
 export const Route = createFileRoute("/sitemap.xml")({
   server: {
